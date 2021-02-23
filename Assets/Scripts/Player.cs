31c4c18010e644e456eaf9379e9ea6da54bb7281
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2021-02-23 08:42:05 +0530

    Game Updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,51 +6,51 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public float speed = 5;
-    private int count = 0;
+    // private int count = 0;
     private Rigidbody rb;
-    public Text scoreText;
-    public Text winText;
+    //public Text scoreText;
+    //public Text winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        DisplayScore();
-        winText.text = "";
+        //DisplayScore();
+        //winText.text = "";
     }
 
-    void Update()
-    {
-        if(count == 5)
-        {
-            winText.text = "You Win !";
-        }
-    }
+    //void Update()
+    //{
+    //    if(count == 5)
+    //    {
+    //        winText.text = "You Win !";
+    //    }
+    //}
 
     void FixedUpdate()
     {
-        //float moveHorizontal = Input.GetAxis("Horizontal");
-        //float moveVartical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVartical = Input.GetAxis("Vertical");
 
-        float moveHorizontal = Input.acceleration.x;
-        float moveVartical = Input.acceleration.y;
+        //float moveHorizontal = Input.acceleration.x;
+        //float moveVartical = Input.acceleration.y;
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVartical);
 
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUps"))
-        {
-            other.gameObject.SetActive(false);
-            count++;
-            DisplayScore();
-        }
-    }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("PickUps"))
+    //    {
+    //        other.gameObject.SetActive(false);
+    //        count++;
+    //        DisplayScore();
+    //    }
+    //}
 
-    void DisplayScore()
-    {
-        scoreText.text = "Score : " + count.ToString();
-    }
+    //void DisplayScore()
+    //{
+    //    scoreText.text = "Score : " + count.ToString();
+    //}
 }
